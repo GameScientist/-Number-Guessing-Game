c7ee9dd8d67d73a7d4663f4c1b8c236527560ce7
Tyler Kortge <65042202+GameScientist@users.noreply.github.com>
2023-03-23 17:22:55 -0400

    Update number_guess.sh

--- number_guess.sh
+++ number_guess.sh
@@ -8,37 +8,43 @@ if [[ -z $USERNAME ]]
 then
   echo -e "Welcome, $NEW_USERNAME! It looks like this is your first time here."
   INSERT=$($PSQL "INSERT INTO players(username, games_played, best_game) VALUES('$NEW_USERNAME', 0, 0)")
+  GAMES=0
+  BEST=0
 else
   GAMES=$($PSQL "SELECT games_played FROM players WHERE username='$NEW_USERNAME'")
   BEST=$($PSQL "SELECT best_game FROM players WHERE username='$NEW_USERNAME'")
   echo -e "Welcome back, $USERNAME! You have played $GAMES games, and your best game took $BEST guesses."
 fi
+
 echo -e "Guess the secret number between 1 and 1000:"
 SECRETNUMBER=$[ $RANDOM % 1000 + 0 ]
 GUESSES=0
 while read GIVENNUMBER
 do
-  if ! [[ $GIVENNUMBER =~ ^[0-9]+$ ]]
+  if [[ $GIVENNUMBER =~ ^[0-9]+$ ]];
   then
-    echo -e "That is not an integer, guess again:"
-  elif [[ $SECRETNUMBER -lt $GIVENNUMBER ]]
-  then
-    echo -e "It's lower than that, guess again:"
     GUESSES=$(($GUESSES+1))
-  elif [[ $SECRETNUMBER -gt $GIVENNUMBER ]]
-  then
-    echo -e "It's higher than that, guess again:"
-    GUESSES=$(($GUESSES+1))
+    if [ $GIVENNUMBER -eq $SECRETNUMBER ];
+    then
+      GAMES=$(($GAMES+1))
+      echo -e "You guessed it in $GUESSES tries. The secret number was $SECRETNUMBER. Nice job!"
+      break;
+    else
+      if [[ $SECRETNUMBER -lt $GIVENNUMBER ]];
+      then
+        echo -e "It's lower than that, guess again:"
+      elif [[ $SECRETNUMBER -gt $GIVENNUMBER ]];
+      then
+        echo -e "It's higher than that, guess again:"
+      fi
+    fi
   else
-    GUESSES=$(($GUESSES+1))
-    break
+    echo -e "That is not an integer, guess again:"
   fi
 done
-GAMES=$(($($PSQL "SELECT games_played FROM players WHERE username='$NEW_USERNAME'")+1))
-UPDATE=$($PSQL "UPDATE players SET games_played = $GAMES WHERE username = '$NEW_USERNAME'")
-BEST=$($PSQL "SELECT best_game FROM players WHERE username='$NEW_USERNAME'")
-if [[ $GUESSES -lt $BEST || $GAMES == 1 ]]
+if (($BEST==0 || $BEST>$GUESSES));
 then
-  EQUAL=$($PSQL "UPDATE players SET best_game = $GUESSES WHERE username = '$NEW_USERNAME'")
+  UPDATE=$($PSQL "UPDATE players SET games_played=$GAMES, best_game=$GUESSES WHERE username = '$NEW_USERNAME'")
+else
+  UPDATE=$($PSQL "UPDATE players SET games_played=$GAMES WHERE username = '$NEW_USERNAME'")
 fi
-echo -e "You guessed it in $GUESSES tries. The secret number was $SECRETNUMBER. Nice job!"
